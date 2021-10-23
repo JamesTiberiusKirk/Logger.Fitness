567564d350372f5ee2451cfc11357c0e6e5f4848
Dumitru Vulpe <dumitru@xiatech.co.uk>
2021-10-23 11:45:26 +0100

    Added go server for web

---
+++ web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+
+	"github.com/fvbock/endless"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Static("/", "public")
+
+	endless.DefaultHammerTime = 10 * time.Second
+	endless.DefaultReadTimeOut = 295 * time.Second
+	if err := endless.ListenAndServe(":5000", e); err != nil {
+		log.Infof("Server stopped: %s", err)
+	}
+}
