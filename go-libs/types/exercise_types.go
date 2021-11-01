561471cf5f09ca15c90726656217b8b908b21bb2
Dumitru Vulpe <dumitru@xiatech.co.uk>
2021-11-01 17:26:42 +0000

    Added exercise types type

---
+++ go-libs/types/exercise_types.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type DataType string
+
+const (
+	Sets        DataType = "sets"
+	SingleValue DataType = "single-value"
+)
+
+type ExerciseType struct {
+	Id          primitive.ObjectID `bson:"_id"`
+	UserId      string             `json:"userId" bson:"userId"`
+	Name        string             `json:"name" bson:"name" validate:""`
+	Description string             `json:"description" bson:"description"`
+	DataType    DataType           `json:"data_type" bson:"data_type"`
+}
+
+func (exerciseType ExerciseType) IsValid() error {
+	validate := validator.New()
+	return validate.Struct(exerciseType)
+}
