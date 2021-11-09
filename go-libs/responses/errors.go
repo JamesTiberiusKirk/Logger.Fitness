7576d42229ea03accfa2a5e3b00a2641825d2c88
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-11-09 12:00:31 +0000

    Added a separate errors.go file for cosntants

---
+++ go-libs/responses/errors.go
@@ -0,0 +1,13 @@
+package res
+
+// Error messages.
+const (
+	Unauthorised        = "Login failed"
+	JwtError            = "JWT error"
+	JwtInvalid          = "JWT invalid"
+	UserExists          = "User already exists"
+	DatabseError        = "Database error"
+	InternalServerError = "Internal server error"
+	BadPayload          = "Badly formatted payload"
+	MissingID           = "Missing ID"
+)
