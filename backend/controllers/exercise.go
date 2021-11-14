e073b6f95858538d21ff04c06a9671c5678bc661
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-11-14 12:14:14 +0000

    Added exercise template

---
+++ backend/controllers/exercise.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"Logger.Fitness/backend/db"
+	"Logger.Fitness/go-libs/types"
+	"github.com/labstack/echo/v4"
+)
+
+/* Exercise */
+
+// TODO: Need to finish all operations on exercises
+
+// NewExercise POST endpoint.
+// Starts new exercise.
+func NewExercise(c *echo.Context) error {
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+	return nil
+}
+
+// DeleteExercise DELETE endpoint.
+// Deletes exercise.
+func DeleteExercise(c *echo.Context) error {
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+	return nil
+}
+
+/* Sets in exercises */
+
+// AddSetInExercise POST endpoint.
+// Added new set in exercise.
+func AddSetInExercise(c *echo.Context) error {
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+	return nil
+}
+
+// NOTE: Maybe the following two can be left out and
+// 	only update either the exercise or the entire workout.
+
+// EditSetInExercise endpoint.
+// Edit an individual set in an exercise.
+// NOTE: Precision specified by array iterator.
+func EditSetInExercise(c *echo.Context) error {
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+	return nil
+}
+
+// DeleteSetInExercise DELETE endpoint.
+// Deletes set in exercise.
+func DeleteSetInExercise(c *echo.Context) error {
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+	return nil
+}
---
+++ go-libs/types/exercise.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// SingleValue for the data in exercises
+type SingleValue struct {
+	DataType string `json:"data_type" bson:"data_type"`
+	Value    int    `json:"value" bson:"value"`
+}
+
+// Set for the data in exercises
+type Set struct {
+	Resps      int  `json:"reps" bson:"reps"`
+	Resistance int  `json:"resistance" bson:"resistance"`
+	IsDropSet  bool `json:"is_drop_set" bson:"is_drop_set"`
+}
+
+// Exercise is for one individual exercise in a workout
+type Exercise struct {
+	ID             primitive.ObjectID `json:"exercise_id" bson:"_id"`
+	WorkoutID      primitive.ObjectID `json:"workout_id" bson:"workout_id"`
+	ExerciseTypeID primitive.ObjectID `json:"exercise_type" bson:"exercise_type"`
+	UserID         primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Sets           []Set              `json:"sets" bson:"sets"`
+	SingleValue    SingleValue        `json:"single_value" bson:"single_value"`
+	Notes          string             `json:"notes" bson:"notes"`
+}
