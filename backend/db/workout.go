f91ce677d368ae2dd218df2633794e389a3b9e35
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-11-16 11:27:34 +0000

    Started working on wodnouts

--- backend/db/workout.go
+++ backend/db/workout.go
@@ -1,31 +1,124 @@
 package db
 
 import (
+	"context"
+	"fmt"
+
 	"Logger.Fitness/go-libs/types"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // WorkoutsCollection const for the mongodb collection
 const WorkoutsCollection = "workouts"
 
-// InsertNewWorkout mongodb insert operation
+// InsertNewWorkout insert operation
 func (db *DbClient) InsertNewWorkout(newWorkout types.Workout) error {
-	log.Info("Test ")
+	dbc := db.Conn
+	collection := dbc.Database(DB_NAME).Collection(WorkoutsCollection)
+
+	err := newWorkout.IsValid()
+	if err != nil {
+		return err
+	}
+
+	newWorkout.ID = primitive.NewObjectID()
+	_, err = collection.InsertOne(context.TODO(), newWorkout)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// GetUserWorkout mongodb get operation
-func (db *DbClient) GetUserWorkout(userID primitive.ObjectID) ([]types.Workout, error) {
-	return nil, nil
-}
-
 // EditWorkout re-write operation
-func (db *DbClient) EditWorkout(types.Workout) error {
+// TODO: maybe I need to find something todo with the result?
+func (db *DbClient) EditWorkout(workout types.Workout) error {
+	dbc := db.Conn
+	collection := dbc.Database(DB_NAME).Collection(WorkoutsCollection)
+
+	filter := bson.M{"_id": workout.ID, "user_id": workout.UserID}
+	update := bson.M{"$set": workout}
+
+	result, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+	log.Info(result)
+
 	return nil
 }
 
 // DeleteWorkout delete operation
 func (db *DbClient) DeleteWorkout(workoutID, userID primitive.ObjectID) error {
+	dbc := db.Conn
+	collection := dbc.Database(DB_NAME).Collection(WorkoutsCollection)
+
+	filter := bson.M{"_id": workoutID, "user_id": userID}
+	_, err := collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
+
+// GetUserWorkout get operation
+func (db *DbClient) GetUserWorkout(userID primitive.ObjectID) ([]types.Workout, error) {
+	dbc := db.Conn
+	collection := dbc.Database(DB_NAME).Collection(WorkoutsCollection)
+
+	if userID.Hex() == "" {
+		return nil, fmt.Errorf("need to provide a userID")
+	}
+
+	var result []types.Workout
+	filter := bson.M{"user_id": userID}
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// GetUserWorkoutByID get operation
+func (db *DbClient) GetUserWorkoutByID(workoutID, userID primitive.ObjectID) (types.Workout, error) {
+	dbc := db.Conn
+	collection := dbc.Database(DB_NAME).Collection(WorkoutsCollection)
+	var result types.Workout
+
+	if userID.Hex() == "" {
+		return result, fmt.Errorf("need to provide a userID")
+	}
+
+	if workoutID.Hex() == "" {
+		return result, fmt.Errorf("need to provide a workoutID")
+	}
+
+	filter := bson.M{"_id": workoutID, "user_id": userID}
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+// GetUserWorkoutByQuery get operation
+// TODO: Need to implement
+// func (db *DbClient) GetUserWorkoutByQuery(qery string, userID primitive.ObjectID) (types.Workout, error) {
+// dbc := db.Conn
+// collection := dbc.Database(DB_NAME).Collection(WorkoutsCollection)
+// var result types.Workout
+
+// if userID.Hex() == "" {
+// return result, fmt.Errorf("need to provide a userID")
+// }
+
+// filter := bson.M{"user_id": userID}
+// err := collection.FindOne(context.TODO(), filter).Decode(&result)
+// if err != nil {
+// return result, err
+// }
+// return result, nil
+// }
--- go-libs/types/workout.go
+++ go-libs/types/workout.go
@@ -3,13 +3,21 @@ package types
 import (
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Workout is for a collection of exercises
 type Workout struct {
 	ID        primitive.ObjectID `json:"workout_id" bson:"_id"`
+	UserID    primitive.ObjectID `json:"user_id" bson:"user_id"`
 	StartTime time.Time          `json:"start_time" bson:"start_time"`
 	EndTime   time.Time          `json:"end_time" bson:"end_time"`
-	Notes     string             `json:"notes" bson:"notes"`
+	Notes     string             `json:"notes" bson:"notes" validation:"required"`
+}
+
+// IsValid checks if instance of User is valid using the validator.
+func (w Workout) IsValid() error {
+	validate := validator.New()
+	return validate.Struct(w)
 }
