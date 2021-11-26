aaa04d0a8f1423d8524c79780466213eea5af700
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-11-26 17:51:09 +0000

    Routed workouts

--- backend/server/server.go
+++ backend/server/server.go
@@ -40,17 +40,19 @@ func Run(dbClient *db.DbClient, port string) {
 }
 
 func initRoutes(e *echo.Echo) *echo.Echo {
-
-	e.GET("/helloworld", controllers.HelloWorld, lfMiddleware.Auth(lfMiddleware.UserRole))
+	userAuth := lfMiddleware.Auth(lfMiddleware.UserRole)
 
 	e.POST("/auth/verify_me", controllers.VerifyMe)
 	e.POST("/auth/register", controllers.Register)
 	e.POST("/auth/login", controllers.Login)
 
-	e.POST("/extp", controllers.NewExerciseType, lfMiddleware.Auth(lfMiddleware.UserRole))
-	e.GET("/extp", controllers.GetExerciseTypes, lfMiddleware.Auth(lfMiddleware.UserRole))
-	e.PUT("/extp", controllers.EditExerciseTypes, lfMiddleware.Auth(lfMiddleware.UserRole))
-	e.DELETE("/extp", controllers.DeleteExerciseType, lfMiddleware.Auth(lfMiddleware.UserRole))
+	e.POST("/extp", controllers.NewExerciseType, userAuth)
+	e.GET("/extp", controllers.GetExerciseTypes, userAuth)
+	e.PUT("/extp", controllers.EditExerciseTypes, userAuth)
+	e.DELETE("/extp", controllers.DeleteExerciseType, userAuth)
+
+	e.POST("/workouts/start", controllers.StartNewWorkout, userAuth)
+	e.POST("/workouts/stop", controllers.StopWorkout, userAuth)
 
 	return e
 }
