d2496702c25dbbb8486131ff02df8582b38d16f1
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-11-26 17:50:41 +0000

    Almost finished workout MVP functionality

--- backend/controllers/workouts.go
+++ backend/controllers/workouts.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
 	"Logger.Fitness/backend/db"
 	res "Logger.Fitness/go-libs/responses"
@@ -34,7 +35,7 @@ func GetActiveWorkout(c echo.Context) error {
 	db := c.Get("db").(*db.DbClient)
 	userClaim := c.Get("user").(*types.JwtClaim)
 
-	result, err := db.GetUserAcitveWorkouts(userClaim.ID)
+	result, err := db.GetUserAcitveWorkout(userClaim.ID)
 	if err != nil {
 		return c.String(http.StatusInternalServerError, res.DatabseError)
 	}
@@ -45,15 +46,16 @@ func GetActiveWorkout(c echo.Context) error {
 // StartNewWorkout POST endpoint.
 // Starts a new workout if there are no other
 // 	workouts active.
+// @body - start_time unix time
 func StartNewWorkout(c echo.Context) error {
 	db := c.Get("db").(*db.DbClient)
 	userClaim := c.Get("user").(*types.JwtClaim)
 
-	activeWorkout, err := db.GetUserAcitveWorkouts(userClaim.ID)
+	activeWorkout, err := db.GetUserAcitveWorkout(userClaim.ID)
 	if err != nil {
 		return c.String(http.StatusInternalServerError, res.DatabseError)
 	}
-	if len(activeWorkout) != 0 {
+	if activeWorkout != nil {
 		return c.String(http.StatusBadRequest, "Need to stop previous workout first")
 	}
 
@@ -67,6 +69,10 @@ func StartNewWorkout(c echo.Context) error {
 	newWorkout.ID = primitive.NewObjectID()
 	newWorkout.EndTime = nil
 
+	var startTime int64
+	// TODO: Bind a single value from the body of the request into startTime
+	newWorkout.StartTime = time.Unix(startTime, 0)
+
 	err = db.InsertNewWorkout(newWorkout)
 
 	return c.NoContent(http.StatusOK)
@@ -75,25 +81,67 @@ func StartNewWorkout(c echo.Context) error {
 // StopWorkout GET endpoint.
 // Stops the active workout.
 // This also leaves an end time time stamp.
+// @param end_time - RFC3339 timestamp
 func StopWorkout(c echo.Context) error {
-	// db := c.Get("db").(*db.DbClient)
-	// userClaim := c.Get("user").(*types.JwtClaim)
-	return nil
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+	endTimeStamp, err := time.Parse(time.RFC3339, c.QueryParam("end_time"))
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	activeWorkout, err := db.GetUserAcitveWorkout(userClaim.ID)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, res.DatabseError)
+	}
+
+	activeWorkout.EndTime = &endTimeStamp
+	err = db.EditWorkout(*activeWorkout)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, res.DatabseError)
+	}
+
+	return c.String(http.StatusOK, "Workout stopped")
 }
 
 // DeleteWorkout DELETE endpoint.
 // Deletes a workout
+// @param workout_id - id of workout to delete
 func DeleteWorkout(c echo.Context) error {
-	// db := c.Get("db").(*db.DbClient)
-	// userClaim := c.Get("user").(*types.JwtClaim)
-	return nil
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+	workoutID, err := primitive.ObjectIDFromHex(c.QueryParam("workout_id"))
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	err = db.DeleteWorkout(workoutID, userClaim.ID)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, res.DatabseError)
+	}
+
+	return c.NoContent(http.StatusOK)
 }
 
 // EditWorkout PUT endpoint.
 // Re-writes a workout.
 // NOTE: This will even re-write blank fields.
+// @body update - types.Workout
 func EditWorkout(c echo.Context) error {
-	// db := c.Get("db").(*db.DbClient)
-	// userClaim := c.Get("user").(*types.JwtClaim)
-	return nil
+	db := c.Get("db").(*db.DbClient)
+	userClaim := c.Get("user").(*types.JwtClaim)
+
+	var update types.Workout
+	err := c.Bind(&update)
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	update.UserID = userClaim.ID
+	err = db.EditWorkout(update)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, res.DatabseError)
+	}
+
+	return c.NoContent(http.StatusOK)
 }
