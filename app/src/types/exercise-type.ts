0d2c10764f49363cd6003ccd6982fb2f1273230b
Dumitru Vulpe <dumitru@xiatech.co.uk>
2022-02-05 10:28:13 +0000

    Created types

---
+++ app/src/types/exercise-type.ts
@@ -0,0 +1,8 @@
+export type ExerciseType = {
+    exercise_type_id: string,
+    // user_id: string,
+    name: string,
+    description: string,
+    data_type: string,
+    measurement_type: string,
+}
---
+++ app/src/types/workout.ts
@@ -0,0 +1,8 @@
+export type Workout = {
+    workout_id: string,
+    // user_id:string,
+    start_time: string,
+    end_time: string,
+    notes: string,
+    title: string,
+}
