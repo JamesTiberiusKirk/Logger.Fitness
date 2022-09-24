a84b3d444ea24c8134faede5d4b76252a4c81d82
Dumitru Vulpe <dumitru@xiatech.co.uk>
2022-09-24 21:14:16 +0100

    Changed go tags

--- go-libs/types/exercise.go
+++ go-libs/types/exercise.go
@@ -7,7 +7,7 @@ import (
 
 // SingleValue for the data in exercises
 type SingleValue struct {
-	DataType string `json:"data_type" bson:"data_type"`
+	DataType string `json:"dataType" bson:"data_type"`
 	Value    int    `json:"value" bson:"value"`
 }
 
@@ -15,17 +15,17 @@ type SingleValue struct {
 type Set struct {
 	Resps      int  `json:"reps" bson:"reps"`
 	Resistance int  `json:"resistance" bson:"resistance"`
-	IsDropSet  bool `json:"is_drop_set" bson:"is_drop_set"`
+	IsDropSet  bool `json:"isDropSet" bson:"is_drop_set"`
 }
 
 // Exercise is for one individual exercise in a workout
 type Exercise struct {
-	ID             primitive.ObjectID `json:"exercise_id" bson:"_id"`
-	WorkoutID      primitive.ObjectID `json:"workout_id" bson:"workout_id"`
-	ExerciseTypeID primitive.ObjectID `json:"exercise_type_id" bson:"exercise_type_id"`
-	UserID         primitive.ObjectID `json:"user_id" bson:"user_id"`
+	ID             primitive.ObjectID `json:"id" bson:"_id"`
+	WorkoutID      primitive.ObjectID `json:"workoutId" bson:"workout_id"`
+	ExerciseTypeID primitive.ObjectID `json:"exerciseTypeId" bson:"exercise_type_id"`
+	UserID         primitive.ObjectID `json:"userId" bson:"user_id"`
 	Sets           *[]Set             `json:"sets" bson:"sets"`
-	SingleValue    *SingleValue       `json:"single_value" bson:"single_value"`
+	SingleValue    *SingleValue       `json:"singleValue" bson:"single_value"`
 	Notes          string             `json:"notes" bson:"notes"`
 }
 
--- go-libs/types/workout.go
+++ go-libs/types/workout.go
@@ -7,10 +7,10 @@ import (
 
 // Workout is for a collection of exercises
 type Workout struct {
-	ID        primitive.ObjectID `json:"workout_id" bson:"_id"`
-	UserID    primitive.ObjectID `json:"user_id" bson:"user_id"`
-	StartTime int64              `json:"start_time" bson:"start_time"`
-	EndTime   int64              `json:"end_time" bson:"end_time"`
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	UserID    primitive.ObjectID `json:"userId" bson:"user_id"`
+	StartTime int64              `json:"startTime" bson:"start_time"`
+	EndTime   int64              `json:"endTime" bson:"end_time"`
 	Notes     string             `json:"notes" bson:"notes"`
 	Title     string             `json:"title" bson:"title" validation:"required"`
 }
